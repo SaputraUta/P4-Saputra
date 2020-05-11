a54a2de63d34900bdf071a06a0d820dd820a721c
SaputraUta <65081151+SaputraUta@users.noreply.github.com>
2020-05-11 16:38:00 +0800

    Create Jobsheet 1 C.c
    Program Array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	int  nim[5]={9,9,7,8,3};
+	printf("Saputra F1B019129 KLP 26");
+	printf("\nData ke-1= %d",nim[0]);
+	printf("\nData ke-2= %d",nim[1]);
+	printf("\nData ke-3= %d",nim[2]);
+	printf("\nData ke-4= %d",nim[3]);
+	printf("\nData ke-5= %d",nim[4]);	
+}
