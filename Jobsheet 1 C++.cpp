d5d0282074d1638d766dac7243f2d5fd940cc305
SaputraUta <65081151+SaputraUta@users.noreply.github.com>
2020-05-11 16:38:55 +0800

    Create Jobsheet 1 C++.cpp
    Program Array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Saputra F1B019129 KLP 26";
+int nim[5]= {9,9,7,8,3};
+	
+	cout<< "\nNim ke-1 adalah " <<nim[0] <<endl;
+	cout<< "\nNim ke-2 adalah " <<nim[1] <<endl;
+	cout<< "\nNim ke-3 adalah " <<nim[2] <<endl;
+	cout<< "\nNim ke-4 adalah " <<nim[3] <<endl;
+	cout<< "\nNim ke-5 adalah " <<nim[4] <<endl;
+}
