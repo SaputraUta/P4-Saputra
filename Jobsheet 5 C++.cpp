61dd0c36ae5bb7c94df12fdfb744e3798a3260c5
SaputraUta <65081151+SaputraUta@users.noreply.github.com>
2020-05-11 16:49:16 +0800

    Create Jobsheet 5 C++.cpp
    Program perkalian matriks dua dimensi dengan menggunakan NIM dua orang Mahasiswa

---
+++ Jobsheet 5 C++.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<<"Saputra F1B019129 KLP 26";
+  int x,y,baris=2,kolom=2, matriks1[1][1], matriks2[1][1], hasil[1][1];
+  cout<<"\nMasukkan nilai matriks pertama: \n";
+  for(x = 0; x < baris; x++){
+    for(y = 0; y < kolom; y++){
+     cin>>matriks1[x][y];
+    }
+  }
+  cout<<"\nMasukkan Nilai matriks kedua: \n";
+  for(x = 0; x < baris; x++){
+    for(y = 0; y < kolom; y++){
+      cin>>matriks2[x][y];
+    }
+  }
+ cout<<"Hasil Perkalian: \n";
+  for(x = 0; x < baris; x++){
+    for(y = 0; y < kolom; y++){
+      hasil[x][y] = matriks1[x][y] * matriks2[x][y];
+      cout<<hasil[x][y];
+      cout<<"\n";
+  }
+}
+  return 0;
+}
